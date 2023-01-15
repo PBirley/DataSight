fbc878165835f32dda0d86feeb6d838166b1ab94
PBirley <pbirley101@gmail.com>
2023-01-15 12:46:48 +0000

    commit before commit

--- my-app/src/components/BackendStreamPage.js
+++ my-app/src/components/BackendStreamPage.js
@@ -2,11 +2,12 @@ import React from 'react'
 // import DataDashboard from './DataDashboard'
 import DataDashboardModular from './DataDashboardMoudlar'
 import LiveStream from './LiveStream.js'
+// import {Button} from '@mui/material/Button';
 
 export default function BackendStreamPage() {
-  const data = [];
   return (
     <div>
+      {/* <Button variant="contained">Hello World</Button> */}
       <LiveStream />
       <DataDashboardModular />
     </div>
