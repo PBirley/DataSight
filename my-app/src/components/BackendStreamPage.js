d25bebdd24cdd03791f1278719640e11c40278fe
PBirley <pbirley101@gmail.com>
2023-01-16 08:23:41 +0000

    commit before adding material ui

--- my-app/src/components/BackendStreamPage.js
+++ my-app/src/components/BackendStreamPage.js
@@ -1,13 +1,10 @@
 import React from 'react'
-// import DataDashboard from './DataDashboard'
 import DataDashboardModular from './DataDashboardMoudlar'
 import LiveStream from './LiveStream.js'
-// import {Button} from '@mui/material/Button';
 
 export default function BackendStreamPage() {
   return (
     <div>
-      {/* <Button variant="contained">Hello World</Button> */}
       <LiveStream />
       <DataDashboardModular />
     </div>
